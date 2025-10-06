f3eec53974ed93dacf886cb2bc445f3f8135e947
Iambiswajit-C <135822545+Iambiswajit-C@users.noreply.github.com>
2025-10-06 14:42:22 +0530

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// Change topbar background on scroll
+window.addEventListener("scroll", function() {
+  const topbar = document.getElementById("topbar");
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    topbar.classList.remove("topbar-transparent");
+    topbar.classList.add("topbar-solid");
+    navbar.classList.add("shrink");
+  } else {
+    topbar.classList.add("topbar-transparent");
+    topbar.classList.remove("topbar-solid");
+    navbar.classList.remove("shrink");
+  }
+});
